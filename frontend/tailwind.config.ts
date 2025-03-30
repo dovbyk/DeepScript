113dfa8e8fd5f13b90252a9c93133ec40f396047
Aryan Gaire <botx711@gmail.com>
2025-03-30 17:11:09 +0530

    Update tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -19,7 +19,7 @@ export default {
 		},
 		extend: {
 			fontFamily: {
-				agency: ['"Agency FB"', "sans-serif"],
+				agency: ["Agency FB", "sans-serif"],
 				sans: ["Montserrat", "sans-serif"],			},
 			colors: {
 				border: 'hsl(var(--border))',
