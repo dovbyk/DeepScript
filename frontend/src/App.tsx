9b5d8b45624c5d4e1cfb2ff7774e28f7a4522f42
Aryan Gaire <botx711@gmail.com>
2025-04-09 19:39:43 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ const App = () => {
 
   useEffect(() => {
     // Simulate loading delay
-    const timeout = setTimeout(() => setLoading(false), 1000);
+    const timeout = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timeout);
   }, []);
 
