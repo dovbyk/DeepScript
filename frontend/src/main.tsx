27fdf70a35225f8e53c7ccee9d2be9adbfc97a48
Aryan Gaire <botx711@gmail.com>
2025-04-09 19:29:42 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,6 @@
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import LoadingScreen from "@/components/LoadingScreen";
 
-
-const root = document.getElementById("root")!;
-const temp = document.createElement("div");
-root.appendChild(temp);
-
-ReactDOM.createRoot(temp).render(<LoadingScreen />);
-
-setTimeout(() => {
-  ReactDOM.createRoot(root).render(<App />);
-}, 1200); // Delay for effect — adjust as needed
+createRoot(document.getElementById("root")!).render(<App />);
