c543482177bf9b3e4356a020c4aee097ac9af178
Aryan Gaire <botx711@gmail.com>
2025-05-20 17:53:52 +0530

    Update font_generator.py

--- backend/font_generator.py
+++ backend/font_generator.py
@@ -69,7 +69,7 @@ def generate_font(png_file_items):
     # Build glyph_map from the provided items.
     glyph_map = {   "0": "0.svg", "1": "1.svg", "2": "2.svg", "3": "3.svg",
     "4": "4.svg", "5": "5.svg", "6": "6.svg", "7": "7.svg", 
-    "8": "8.svg", "9": "9.svg", ".": "..svg", ",": ",.svg",
+    "8": "8.svg", "9": "9.svg", ".": "$.svg", ",": "#.svg",
     "-": "-.svg", "(": "(.svg", ")": ").svg", "+": "+.svg",
     "!": "!.svg", "?": "?.svg" 
     }
