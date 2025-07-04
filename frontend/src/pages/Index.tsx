66ad1c1d1ec935cb026ec54753ad288c8a5fa161
Aryan Sharma Gaire <botx711@gmail.com>
2025-07-04 12:40:39 +0530

    Update Index.tsx

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -135,7 +135,7 @@ const Index = () => {
               <img 
                 src="/github.jpg" 
                 alt="GitHub" 
-                className="w-10 h-10 hover:opacity-80 transition-opacity"
+                className="w-9 h-9 hover:opacity-80 transition-opacity"
               />
           </a>
           
@@ -146,7 +146,7 @@ const Index = () => {
               className="max-w-[min(100%,800px)] w-auto h-[clamp(80px,20vmin,200px)] md:h-[clamp(100px,25vmin,300px)]"
             />
           </div>
-          <h2 className="font-agency font-bold text-3xl md:text-5xl text-center mb-6">
+          <h2 className="font-agency font-bold text-4xl md:text-6xl text-center mb-6">
             TRANSFORM ANY TEXT INTO A REALISTIC HANDWRITING
           </h2>
         </header>
